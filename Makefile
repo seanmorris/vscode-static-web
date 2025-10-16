d7875f09f7d4cee95248bc09ade69cafe8cde7a7
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-16 12:38:10 -0400

    Tweaks.

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
-VSCODE_TAG=1.89.0
-VSCODE_SKIP_EXTENSIONS=
+-include .env
+
+VSCODE_TAG?=1.89.0
+VSCODE_SKIP_EXTENSIONS?=
+VSCODE_BASEPATH?=
+
+VSCODE_RESOURCE_URL_TEMPLATE?=
+VSCODE_SERVICE_URL?=
+VSCODE_ITEM_URL?=
 
 .PHONY: all serve clean extensions
 
 all: public/index.html
+
+SHELL=/bin/bash -euxo pipefail
 
 ## Download the code ##
 third_party/vscode/.gitignore:
@@ -38,15 +47,21 @@ journal/.static-build: journal/.compiled
 	@ echo "\033[33;4mBuilding Static Distribution...\033[0m"
 	cd public && rm -rf out node_modules resources extensions;
 	cd third_party/vscode && {\
-		cp -rfv node_modules resources extensions ../../public;\
-		cp -rfv out ../../public/out;\
+		cp -prfv node_modules resources extensions ../../public;\
+		cp -prfv out ../../public/out;\
+		echo -n ${VSCODE_SKIP_EXTENSIONS} | xargs -d ' ' -I{} rm -rfv ../../public/extensions/{};\
 	}
 	touch journal/.static-build
 
 ## Build the index.html file: ##
 public/index.html: journal/.static-build source/index-template.html.php extensions
 	@ echo "\033[33;4mGenerating index.html file...\033[0m"
-	VSCODE_SKIP_EXTENSIONS=${VSCODE_SKIP_EXTENSIONS} php source/index-template.html.php > public/index.html;
+	VSCODE_BASEPATH=${VSCODE_BASEPATH} \
+	VSCODE_SKIP_EXTENSIONS=${VSCODE_SKIP_EXTENSIONS} \
+	VSCODE_RESOURCE_URL_TEMPLATE=${VSCODE_RESOURCE_URL_TEMPLATE} \
+	VSCODE_SERVICE_URL=${VSCODE_SERVICE_URL} \
+	VSCODE_ITEM_URL=${VSCODE_ITEM_URL} \
+	php source/index-template.html.php > public/index.html;
 
 ## Clean the repo: ##
 clean:
@@ -58,9 +73,10 @@ clean-static:
 
 ## Run the testing server: ##
 serve: all
-	cd public/ && npx http-server
+	cd public/ && http-server
 	
 ## Copy extra extensions to public/extensions/ ##
 extensions: journal/.static-build
 	- cp -rf extra_extensions/* public/extensions/
+	echo -n ${VSCODE_SKIP_EXTENSIONS} | xargs -d ' ' -I{} rm -rf ./public/extensions/{};\
 	touch journal/.extensions
--- source/index-template.html.php
+++ source/index-template.html.php
@@ -2,7 +2,17 @@
 <!-- Copyright (C) Microsoft Corporation. All rights reserved. -->
 <!-- Modifications for EDUCATIONAL PURPOSES by Sean Morris. -->
 <?php
+$basePath = getenv('VSCODE_BASEPATH') ?: '';
 $skipExtensions = explode(',', getenv('VSCODE_SKIP_EXTENSIONS'));
+
+$resourceUrlTemplate = getenv('VSCODE_RESOURCE_URL_TEMPLATE')
+	?: 'https://open-vsx.org/vscode/asset/{publisher}/{name}/{version}/Microsoft.VisualStudio.Code.WebResources/{path}';
+
+$serviceUrl = getenv('VSCODE_SERVICE_URL')
+	?: 'https://open-vsx.org/vscode/gallery';
+
+$itemUrl = getenv('VSCODE_ITEM_URL')
+	?: 'https://open-vsx.org/vscode/item';
 
 $extDirs = array_filter(
 	scanDir('./public/extensions')
@@ -35,9 +45,14 @@ $hacks = array_map(
 	, array_values($extDirs)
 );
 
-$packages = array_map(
-	function($name)
+$packages = array_filter(array_map(
+	function($name) use($skipExtensions)
 	{
+		if(in_array($name, $skipExtensions))
+		{
+			return false;
+		}
+
 		$packageJSONFile = './public/extensions/' . $name . '/package.json';
 		$packageNLSFile  = './public/extensions/' . $name . '/package.nls.json';
 
@@ -60,7 +75,7 @@ $packages = array_map(
 		return $entry;
 	}
 	, array_values($extDirs)
-); ?>
+)); ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -68,9 +83,11 @@ $packages = array_map(
 
 		<!-- Disable pinch zooming -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+		<base href="<?=$basePath?>">
 
 		<!-- Workbench Configuration -->
 		<meta id="vscode-workbench-web-configuration" data-settings="{
+			&quot;serverBasePath&quot;: &quot;<?=$basePath?>/&quot;,
 			&quot;initialColorTheme&quot;: {
 				&quot;themeType&quot;: &quot;dark&quot;
 			},
@@ -82,7 +99,12 @@ $packages = array_map(
 				&quot;path&quot;:&quot;/default.code-workspace&quot;,
 				&quot;scheme&quot;:&quot;tmp&quot;},
 				&quot;productConfiguration&quot;:{
-					&quot;enableTelemetry&quot;:false
+					&quot;enableTelemetry&quot;:false,
+					&quot;extensionsGallery&quot;: {
+						&quot;resourceUrlTemplate&quot;: &quot;<?=$resourceUrlTemplate?>&quot;,
+						&quot;serviceUrl&quot;: &quot;<?=$serviceUrl?>&quot;,
+						&quot;itemUrl&quot;: &quot;<?=$itemUrl?>&quot;
+					}
 				}
 			}"
 		>
@@ -94,7 +116,21 @@ foreach($packageHacks as $hack):?>
 			vscodeAlterConfigCallbacks.push(<?php echo file_get_contents($hack); ?>);
 <?php endforeach; endforeach; ?>
 
-			window.vscodeAlterConfig = config => vscodeAlterConfigCallbacks.map(callback => callback(config));
+			window.vscodeAlterConfig = config => {
+				config.commands = config.commands || [];
+				vscodeAlterConfigCallbacks.map(callback => callback(config));
+
+				const searchParams = new URLSearchParams(location.search);
+
+				if(searchParams.has('itemUrl'))
+					config.productConfiguration.extensionsGallery.itemUrl = searchParams.get('itemUrl');
+
+				if(searchParams.has('serviceUrl'))
+					config.productConfiguration.extensionsGallery.serviceUrl = searchParams.get('serviceUrl');
+
+				if(searchParams.has('resourceUrlTemplate'))
+					config.productConfiguration.extensionsGallery.resourceUrlTemplate = searchParams.get('resourceUrlTemplate');
+			};
 		</script>
 
 		<!-- Builtin Extensions -->
@@ -107,17 +143,18 @@ foreach($packageHacks as $hack):?>
 
 		<!-- Workbench Icon/Manifest/CSS -->
 		<link rel="icon" href="/favicon.ico" type="image/x-icon" />
-		<link data-name="vs/workbench/workbench.web.main" rel="stylesheet" href="/out/vs/workbench/workbench.web.main.css" />
+		<link data-name="vs/workbench/workbench.web.main" rel="stylesheet" href="./out/vs/workbench/workbench.web.main.css" />
 	</head>
 
 	<body aria-label="">
+		<div id = "loading-status" style = "position: absolute; z-index: -1; top: 0; left: 0; width: 100%; height: 100%; display: flex; flex-direction: column; justify-content: center; align-items:flex-start; white-space: pre; overflow: hidden; font-size: 1rem; font-family: monospace; padding: 1rem; box-sizing: border-box; background: black; color: white;"></div>
 	</body>
 
 	<!-- Startup (do not modify order of script tags!) -->
-	<script src="/out/vs/loader.js"></script>
-	<script src="/out/vs/webPackagePaths.js"></script>
+	<script src="./out/vs/loader.js"></script>
+	<script src="./out/vs/webPackagePaths.js"></script>
 	<script>
-		let baseUrl = window.origin
+		let baseUrl = `${window.origin}<?=$basePath?>`
 		Object.keys(self.webPackagePaths).map(function (key, index) {
 			self.webPackagePaths[key] = `${baseUrl}/node_modules/${key}/${self.webPackagePaths[key]}`;
 		});
@@ -134,6 +171,36 @@ foreach($packageHacks as $hack):?>
 			}),
 			paths: self.webPackagePaths
 		});
+		const status = document.getElementById('loading-status');
+		const observer = new MutationObserver((mutationList, observer) => {
+			for(const mutation of mutationList)
+			{
+				if(mutation.type === "childList")
+				{
+					mutation.addedNodes.forEach(element => {
+						const prev = status.innerText;
+						switch(element.tagName)
+						{
+							case 'SCRIPT':
+								// console.log(element.getAttribute('src'));
+								status.innerText = "Loading " + element.getAttribute('src');
+								break
+							
+							case 'LINK':
+								// console.log(element.getAttribute('href'));
+								// status.innerText = "Loading " + element.getAttribute('href') + "\n" + prev;
+								break;
+								
+							default:
+								// console.log(element);
+								break;
+						}
+					});
+				}
+			}
+		});
+		observer.observe(document.head, { attributes: true, childList: true, subtree: true });
+		// observer.disconnect();
 	</script>
 	<script> require(['vs/code/browser/workbench/workbench'], function() {}); </script>
 </html>
